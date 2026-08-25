60a5309a376520ba37a689ca10132d175bddac0e
juanm-caceres24 <juanmanuel.caceres24@outlook.com>
2026-08-25 01:14:48 -0300

    Implemented buttons in blinking leds

--- FPGA-BlinkTest/top.v
+++ FPGA-BlinkTest/top.v
@@ -1,12 +1,28 @@
 module top (
     input  wire       clk,
+    input  wire       btn1,
+    input  wire       btn2,
     output wire [5:0] leds
 );
-    reg [23:0] counter = 24'd0;
+
+    reg [31:0] counter = 32'd0;
 
     always @(posedge clk) begin
         counter <= counter + 1'b1;
     end
 
-    assign leds = ~counter[23:18];
+    reg [5:0] current_speed;
+
+    always @(*) begin
+        
+        case ({btn1, btn2})
+            2'b00: current_speed = counter[28:23];
+            2'b10: current_speed = counter[27:22];
+            2'b01: current_speed = counter[26:21];
+            2'b11: current_speed = counter[25:20];
+        endcase
+    end
+
+    assign leds = ~current_speed;
+
 endmodule
